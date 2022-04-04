3be2ac97c3c03084de57aa5dda762cbfe0d4f878
IRMuhammadjon <102463648+IRMuhammadjon@users.noreply.github.com>
2022-04-04 09:14:21 +0500

    Добавьте файлы проекта.

---
+++ GITA_exam_10MASALA/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("1-sonni kiriting: ");
+var a=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("2-sonni kiriting: ");
+var b = Convert.ToInt32(Console.ReadLine());
+while (a!=0 && b!=0)
+{
+    if(a>b)
+    {
+        a = a % b;
+    }
+    else
+    {
+        b= b % a;
+    }
+}
+Console.WriteLine(a + b);
